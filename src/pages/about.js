5ab7a052227df29116a049a22ca959fd63687028
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2018-11-10 19:05:47 -0500

    Added return home buttons on both about and contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Button from '@material-ui/core/Button'
 
 const About = () => (
     <div>
         About page
+        <Button>
+        <Link to="/"
+         style={{
+            textDecoration: 'none',
+          }}>
+        Home
+        </Link>
+        </Button>
     </div>
 )
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Button from '@material-ui/core/Button'
 
 const Contact = () => (
     <div>
         Contact page
+    <Button>
+        <Link to="/"
+         style={{
+            textDecoration: 'none',
+          }}>
+        Home
+        </Link>
+    </Button>
+
     </div>
 )
 
